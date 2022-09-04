24f6ca25c93abd2486494bee45945057dee78593
Felipe Prado <jfpc11@misena.edu.co>
2022-09-04 18:16:50 -0500

    initialazing the project with react-redux

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Container } from '@mui/material';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container maxWidth="md"></Container>
+    </div>
+  );
+}
+
+export default App;
